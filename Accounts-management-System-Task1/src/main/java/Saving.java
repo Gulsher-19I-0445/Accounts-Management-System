d3e33567258f019484ced88414777ebbde2cfda3
Gulsher <gulsherkhan962@gmail.com>
2021-09-11 16:00:20 +0500

    Added Saving Account feature and fixed issues with sign up

---
+++ Accounts-management-System-Task1/src/main/java/Saving.java
@@ -0,0 +1,33 @@
+
+public class Saving extends Accounts {
+	
+	public Saving() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Saving(int A, int num) {
+		super(A, num);
+		// TODO Auto-generated constructor stub
+	}
+	//--------------------------------------------------------
+	//-----------------WithDraw-------------------------------
+	
+	public void withdraw(int amt) {
+		int temp=0;
+		temp=account_balance-amt;
+		if(temp<0) {
+			System.out.println("You are exceeding your current balance limits for you Saving Account. WithDrawal failed. Current Balance: Rs"+account_balance);
+		}
+		else {
+			account_balance=account_balance-amt;
+		}
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Accounts-management-System-Task1/src/main/java/UI.java
+++ Accounts-management-System-Task1/src/main/java/UI.java
@@ -1,3 +1,4 @@
+import java.rmi.server.Operation;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,6 +9,9 @@ public class UI {
 	//----------------------------------------------------------------
 	private String UserNames;
 	private ArrayList<Checking> a=new ArrayList<Checking>();
+	private ArrayList<Saving> s=new ArrayList<Saving>();
+	int unique1;
+	int unique2;
 	//Accounts a=new Accounts();
 	
 	
@@ -17,7 +21,9 @@ public class UI {
 	//-----------------------------------------------------------------
 	public UI() {
 		//a.add(new Accounts());
-		System.out.println("Hello");
+		//System.out.println("Hello");
+		unique1=0;
+		unique2=0;
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -52,6 +58,7 @@ public class UI {
 				if(opt=='B'||opt=='b') {
 					int am;
 					Scanner i2=new Scanner(System.in);
+					System.out.println("Enter the amount you want to withDraw");
 					am=i2.nextInt();
 					b.withdraw(am);
 				}
@@ -59,7 +66,45 @@ public class UI {
 				if(opt=='C'||opt=='c') {
 				b.checkBalance();
 				}
+				
 			}
+			//-----------------------------
+			if(atyp=='Y'||atyp=='y') {
+				Saving c=(Saving)s.get(i);
+				System.out.println("Select what you want to do");
+				System.out.println("(A) Deposit Money");
+				System.out.println("(B) Withdraw Money");
+				System.out.println("(C)Check Balance");
+				System.out.println("(D)PrintStatement");
+				System.out.println("(E)Transfer Money");
+				System.out.println("(F)Calculate Zakat");
+				System.out.println("(G)Display all deductions");
+				Scanner i1=new Scanner(System.in);
+				opt=i1.next().charAt(0);
+				if(opt=='A'||opt=='a') {
+					c.makeDeposit();
+					//if(b.free_count==0) {
+					//	System.out.println("\nYour free monthly deposit has ended");
+						//b.deduction();
+					//}
+					//b.free_count--;
+					}
+				
+				if(opt=='B'||opt=='b') {
+					int am;
+					Scanner i2=new Scanner(System.in);
+					System.out.println("Enter the amount you want to withDraw");
+					am=i2.nextInt();
+					c.withdraw(am);
+				}
+				
+				if(opt=='C'||opt=='c') {
+				c.checkBalance();
+				}
+				
+				
+			}
+			
 		}
 			
 		}
@@ -70,15 +115,19 @@ public class UI {
 	//----------------------------------------------------------------
 	//----------------------Sign Up-----------------------------------
 	//--------------------Create Account------------------------------
-	public void SignUp(int unique) {			//Create Account Module
+	public void SignUp() {			//Create Account Module
 		Scanner i1=new Scanner(System.in);
 		System.out.println("Select Account type");
 		System.out.println("Press X for Checking");
 		System.out.println("Press Y for Saving");
 		char AccT=i1.next().charAt(0);
 		if(AccT=='X'||AccT=='x') {
-			a.add(new Checking(AccT,unique));
-			Operations(unique,AccT);
+			a.add(new Checking(AccT,unique1));
+			Operations(unique1,AccT);
+		}
+		else if(AccT=='Y'||AccT=='y') {
+			s.add(new Saving(AccT,unique2));
+			Operations(unique2,AccT);
 		}
 		//System.out.println("Enter Account Number");
 		//int accNum=i1.nextInt();
@@ -112,7 +161,7 @@ public class UI {
 		System.out.println("Select the Options below to proceed");
 		option=i1.next().charAt(0);
 		if(option=='a'||option=='A')
-		interface1.SignUp(id);
+		interface1.SignUp();
 		
 		//interface1.Show(id);
 		id++;
